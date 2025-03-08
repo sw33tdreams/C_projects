2813203fdb6a25a9dfd56ae880aa1d6faae7eae4
ArtyomHakobyan <artyom.hakobyanshnik@gmail.com>
2025-03-08 23:21:27 +0400

    added C program

---
+++ C1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	double a=0;
+	double b=0;
+	printf("add first number \n");
+	scanf("%lf",&a);
+	printf("add second number \n");
+	scanf("%lf",&b);
+	double c = a + b;
+	printf("sum is %lf",c);
+	return 0;}
