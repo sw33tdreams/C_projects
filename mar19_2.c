32e96083449db46c72efc9ef9bbd8a74a2baa2ea
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-03-19 21:20:01 +0400

    5in bajanvox tveri hashvvum

---
+++ mar19_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	int b = 0;
+	int l = 0;
+	printf("enter 2 integer numbers:\n");
+	scanf("%d %d",&a,&b);
+	
+	for(int i = a; i <= b; i++){
+	if(i % 5 == 0){
+	l++;
+	}
+	}
+	printf("theresult is %d \n", l); 
+	return 0;
+}
