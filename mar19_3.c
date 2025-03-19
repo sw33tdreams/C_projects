26eac727b9fe3c6fb49af7844de99ed46259a6fc
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-03-19 21:45:00 +0400

    bizz-fizz-bizzfizz program

---
+++ mar19_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	int a = 0;
+	for (int i = a; i <= 100; i++ ){
+	if(i % 3 == 0)
+	printf("fizz\n");
+	else if(i % 5 == 0)
+	printf("bizz\n");
+	else if(i % 3 == 0 && i % 5 == 0)
+	printf("fizzbizz\n");
+	else printf("%d\n",i);
+	}
+	return 0;
+}
