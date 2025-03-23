9de3bd7e055902fb8b4eddb241ad94210416b88f
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-03-23 17:59:04 +0400

    bitwise operators

--- bitwise12.c
+++ bitwise12.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 int main(){
-
+int num1 = 0;
+int num2 = 0;
+int x = 0;
+printf("enter two int nums:   ");
+scanf("%d",&num1);
+scanf("%d",&num2);
+printf("choose your operation & | ^\n")
+scanf("%d",&x);
+switch(x){
+    case 1:
+    num1 = num1 & num2;
+    printf("res is %d",num1);
+    case 2:
+    num1 = num1 
