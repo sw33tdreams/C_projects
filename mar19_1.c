5930750be65237366b15f5f24d1b81defed907a2
sw33tdreams <artyom.hakobyanshnik@gmail.com>
2025-03-25 19:24:12 +0400

    fixed issues added prefix

--- mar19_1.c
+++ mar19_1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	for(int i = 0; i <= 10; i++){
+	for(int i = 0; i <= 10; ++i){
 		printf("%d\n",i);
 	}
 	int i = 10;
 	while(i >= 0){
 		printf("%d\n",i);
-		i--;
+		--i;
 	}
 return 0;
 }
